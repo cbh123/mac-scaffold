f1b4744bc92a78c28add0bec026ba127120352fb
Charlie Holtz <charlieholtz@gmail.com>
2023-12-10 13:34:56 -0800

    Initial Commit

---
+++ MacScaffold/MacScaffoldApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacScaffoldApp.swift
+//  MacScaffold
+//
+//  Created by Charlie Holtz on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MacScaffoldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
